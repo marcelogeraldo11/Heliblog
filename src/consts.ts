9b99dd81ad11486197e7ab47ac535628387ca98e
marcelogeraldo11 <m.geraldo11.11@gmail.com>
2025-08-06 11:48:11 -0400

    Cambiar título del blog de 'a minimalistic blog' a 'Blog de Heliboss'

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my blog built with Astro';
+export const SITE_TITLE = 'Blog de Heliboss';
+export const SITE_DESCRIPTION = 'Blog de Heliboss - Tecnología de drones y cámaras DJI';
